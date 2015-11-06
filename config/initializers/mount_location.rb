0d8dfb316cc54592197af150a622edf61854f0a6
Vladimir Tamara <vtamara@pasosdeJesus.org>
2015-11-06 09:41:48 -0500

    localizacion recursos

--- config/initializers/mount_location.rb
+++ config/initializers/mount_location.rb
@@ -1,2 +1,2 @@
 Sivel2Anzorc::Application.config.relative_url_root = '/anzorc/sivel2'
-Sivel2Anzorc::Application.config.assets.prefix = '/anzorc/sivel2'
+Sivel2Anzorc::Application.config.assets.prefix = '/anzorc/sivel2/assets'
