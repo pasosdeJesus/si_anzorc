a0db6f57b491719755e78e762a5a37504940066c
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-03-31 10:59:12 -0500

    actualiza a adJ 5.8 (doas en lugar de sudo, socket de PostgreSQL reubicado),  con ruby 2.3 y gemas

--- bin/u.sh
+++ bin/u.sh
@@ -8,7 +8,11 @@ if (test "${USUARIO_AP}" = "") then {
 	echo "Definir usuario con el que se ejecuta en USUARIO_AP"
 	exit 1;
 } fi;
-sudo su ${USUARIO_AP} -c "cd /var/www/htdocs/sivel2_anzorc; rake assets:precompile; echo \"Iniciando unicorn...\"; SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec unicorn_rails -c ../sivel2_anzorc/config/unicorn.conf.minimal.rb  -E production -D"
+doas=`which doas 2> /dev/null`
+if (test "$?" != "0") then {
+	doas="sudo"
+} fi;
+$doas su ${USUARIO_AP} -c "cd /var/www/htdocs/sivel2_anzorc; rake assets:precompile; echo \"Iniciando unicorn...\"; SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec unicorn_rails -c ../sivel2_anzorc/config/unicorn.conf.minimal.rb  -E production -D"
 
 
   
