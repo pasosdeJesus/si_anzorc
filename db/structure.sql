1e72df005a260db2467dd621dc33391a608d49ff
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-05-30 21:51:37 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -3552,3 +3552,5 @@ INSERT INTO schema_migrations (version) VALUES ('20160316100625');
 
 INSERT INTO schema_migrations (version) VALUES ('20160316100626');
 
+INSERT INTO schema_migrations (version) VALUES ('20160519195544');
+
