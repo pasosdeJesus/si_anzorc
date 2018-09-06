8c21c03e2e1d1d63f75509dbd055bc267afb523a
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-09-06 07:03:06 -0500

    Actualiza

--- test/integration/acceso_test.rb
+++ test/integration/acceso_test.rb
@@ -15,7 +15,7 @@ class AccesoTest < Capybara::Rails::TestCase
     assert page.has_content?("Administrar")
 
     visit File.join(Rails.configuration.relative_url_root, '/casos/nuevo')
-    @numcaso=find_field('Código').value
+    @numcaso=find_field('Caso No').value
 
     # Datos básicos
     fill_in "Fecha del hecho", with: '2014-08-05'
