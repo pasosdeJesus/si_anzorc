137d75a3d4d3b93901361d498c856a00fe3a4870
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-12-10 07:46:11 -0500

    Reubica recursos

--- bin/corre.sh
+++ bin/corre.sh
@@ -5,10 +5,22 @@ if (test -f ".env") then {
 	. .env
 } fi;
 if (test "$RC" = "") then {
-	export RC=sivel2anzorc
+	export RC=sivel2
+} fi;
+if (test "$RAILS_ENV" = "") then {
+	RAILS_ENV=development
+} fi;
+if (test "$PUERTODES" = "") then {
+	PUERTODES=2300
+} fi;
+if (test "$IPDES" = "") then {
+	IPDES=127.0.0.1
 } fi;
 if (test "$RAILS_ENV" = "development") then {
-	PORT=$PUERTODES bin/rails s -p $PUERTODES -b $IPDES
+	if (test "$SININD" = "") then {
+		bundle exec rake sip:indices
+	} fi;
+	bin/rails s -p $PUERTODES -b $IPDES
 } else {
 	if (test ! -f /etc/rc.d/$RC) then {
 		echo "Falta script /etc/rc.d/$RC"
--- bin/detiene.sh
+++ bin/detiene.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-# Inicia servicio
+# Detiene servicio
 
 if (test -f ".env") then {
 	. .env
 } fi;
 if (test "$RC" = "") then {
-	RC=sivel2anzorc
+	RC=sivel2
 } fi;
 if (test "$RAILS_ENV" = "production") then {
 	if (test ! -f /etc/rc.d/$RC) then {
 		echo "Falta script /etc/rc.d/$RC"
 		exit 1;
 	} fi;
+
 	doas sh /etc/rc.d/$RC -d stop
 } fi;
 
