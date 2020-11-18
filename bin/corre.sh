90b1f25d7fac9e4ceae73fc12da06d6006b7ecd1
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-11-18 12:44:04 -0500

    corre

--- bin/corre.sh
+++ bin/corre.sh
@@ -2,31 +2,32 @@
 # Inicia servicio
 
 if (test -f ".env") then {
-	. .env
+  . .env
 } fi;
 if (test "$RC" = "") then {
-	export RC=sivel2
+  export RC=sivel2
 } fi;
 if (test "$RAILS_ENV" = "") then {
-	RAILS_ENV=development
+  RAILS_ENV=development
 } fi;
 if (test "$PUERTODES" = "") then {
-	PUERTODES=2300
+  PUERTODES=2300
 } fi;
 if (test "$IPDES" = "") then {
-	IPDES=127.0.0.1
+  IPDES=127.0.0.1
 } fi;
 if (test "$RAILS_ENV" = "development") then {
-	if (test "$SININD" = "") then {
-		bundle exec rake sip:indices
-	} fi;
-	bin/rails s -p $PUERTODES -b $IPDES
+  if (test "$SININD" = "") then {
+    bundle exec rake sip:indices
+    bin/rails assets:precompile --trace
+  } fi;
+  bin/rails s -p $PUERTODES -b $IPDES
 } else {
-	if (test ! -f /etc/rc.d/$RC) then {
-		echo "Falta script /etc/rc.d/$RC"
-		exit 1;
-	} fi;
-	doas sh /etc/rc.d/$RC -d start
+  if (test ! -f /etc/rc.d/$RC) then {
+    echo "Falta script /etc/rc.d/$RC"
+    exit 1;
+  } fi;
+  doas sh /etc/rc.d/$RC -d start
 } fi;
 
 
