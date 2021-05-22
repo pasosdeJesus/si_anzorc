fcf9c456a13678430010fe8e6f7c9640375ab931
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-05-22 15:51:08 -0500

    Actualiza webpacker y expose-loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,34 +1,22 @@
 const { environment } = require('@rails/webpacker')
-
 const webpack = require('webpack')
 
 environment.plugins.prepend(
-    'Provide',
-    new webpack.ProvidePlugin({
-         $: 'jquery',
-          jQuery: 'jquery',
-          jquery: 'jquery',
-          'window.jquery': 'jquery',
-          Popper: ['popper.js', 'default'],
-        })
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
 )
 
-environment.loaders.append('expose', {
-      test: require.resolve('jquery'),
-      use: [
-                  { loader: 'expose-loader', options: '$' },
-                  { loader: 'expose-loader', options: 'jQuery' },
-                ]
+environment.loaders.append('jquery', {
+  test: require.resolve('jquery'),
+  loader: 'expose-loader',
+  options: {
+    exposes: ['$', 'jQuery']
+  }
 })
-
-
-// Solucion de https://github.com/tannerlinsley/react-table/discussions/2048
-// para usar react-table con rails
-const nodeModulesLoader = environment.loaders.get('nodeModules');
-if (!Array.isArray(nodeModulesLoader.exclude)) {
-    nodeModulesLoader.exclude = nodeModulesLoader.exclude == null ? [] : [nodeModulesLoader.exclude];
-}
-
-nodeModulesLoader.exclude.push(/react-table/);
 
 module.exports = environment
