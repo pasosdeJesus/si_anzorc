005cb2894c5d7b6e582550ccef7938d174e752e4
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-05-28 07:43:48 -0500

    Usa ENV.fetch

--- config/application.rb
+++ config/application.rb
@@ -31,36 +31,40 @@ module Cor1440
 
     config.active_record.schema_format = :sql
 
-    config.hosts <<  (ENV['CONFIG_HOSTS'] && ENV['CONFIG_HOSTS'] != '' ? 
-                      ENV['CONFIG_HOSTS'].downcase : 
-                      'defensor.info'.downcase)
+    config.hosts <<  ENV.fetch('CONFIG_HOSTS', 'defensor.info').downcase
     puts "OJO application.rb config.hosts='#{config.hosts}'"
 
-    config.relative_url_root = ENV['RUTA_RELATIVA'] || "/anzorc/si"
-    puts "OJO application.rb config.x.relative_url_root="\
-      "#{config.x.relative_url_root}"
+    config.relative_url_root = ENV.fetch('RUTA_RELATIVA', "/anzorc/si")
+    puts "OJO application.rb config.relative_url_root="\
+      "#{config.relative_url_root}"
 
     # sip
-    config.x.formato_fecha = ENV['FORMATO_FECHA'] || 'dd/M/yyyy'
+    config.x.formato_fecha = ENV.fetch('FORMATO_FECHA', 'dd/M/yyyy')
     puts "OJO application.rb config.x.formato_fecha=#{config.x.formato_fecha}"
 
     # heb412
-    config.x.heb412_ruta = ENV['HEB412_RUTA'] && ENV['HEB412_RUTA'] != '' ?
-      Pathname(ENV['HEB412_RUTA']) : Rails.root.join('public', 'heb412')
+    config.x.heb412_ruta = Pathname(
+      ENV.fetch('HEB412_RUTA', Rails.root.join('public', 'heb412').to_s)
+    )
     puts "OJO application.rb config.x.heb412_ruta=#{config.x.heb412_ruta}"
 
     # sivel2
-    
-    config.x.sivel2_consulta_web_publica = ENV['SIVEL2_CONSWEB_PUBLICA'] &&
-      ENV['SIVEL2_CONSWEB_PUBLICA'] != ''
 
-    config.x.sivel2_consweb_max = ENV['SIVEL2_CONSWEB_MAX'] || 2000
+    config.x.sivel2_consulta_web_publica = 
+      (ENV['SIVEL2_CONSWEB_PUBLICA'] && ENV['SIVEL2_CONSWEB_PUBLICA'] != '')
 
-    config.x.sivel2_consweb_epilogo = ENV['SIVEL2_CONSWEB_EPILOGO'] ?
-      ENV['SIVEL2_CONSWEB_EPILOGO'].html_safe :
-      "<br>Si requiere más puede suscribirse a SIVeL Pro".html_safe
+    config.x.sivel2_consweb_max = ENV.fetch('SIVEL2_CONSWEB_MAX', 2000)
 
-    config.x.sivel2_mapaosm_diasatras = ENV['SIVEL2_CONSWEB_EPILOGO'] || 182
+    config.x.sivel2_consweb_pie = ENV.fetch('SIVEL2_CONSWEB_PIE', '')
+
+    config.x.sivel2_consweb_epilogo = ENV.fetch(
+      'SIVEL2_CONSWEB_EPILOGO', 
+      "<br>Si requiere más puede suscribirse a SIVeL Pro"
+    ).html_safe
+
+    config.x.sivel2_mapaosm_diasatras = ENV.fetch(
+      'SIVEL2_MAPAOSM_DIASATRAS', 182
+    )
 
    end
 end
